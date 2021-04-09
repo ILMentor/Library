6f03d43f3384893bf52d18e108eb1d535b1c464c
ILMentor <dekstroer8202@gmail.com>
2021-04-09 11:15:48 +0200

    fix Cannot resolve MVC view 'index'

--- src/main/java/empire/of/lord/library/controllers/booksController.java
+++ src/main/java/empire/of/lord/library/controllers/booksController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class booksController {
-    // todo fix problem Cannot resolve MVC view 'index'
+
     @RequestMapping("/")
     public String index(){
         return "index";
